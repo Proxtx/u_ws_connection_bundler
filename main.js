98699dcbb1ad1efb07195fece402cc2d34beb989
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-09-22 15:49:21 +0200

    commit to look at the orders when needed

--- main.js
+++ main.js
@@ -2,11 +2,85 @@ import { WebSocketServer } from "ws";
 
 const ws = new WebSocketServer({ port: 8080 });
 
-ws.on("connection", (ws) => {
+ws.on("connection", async (ws) => {
   ws.on("message", (data) => {
     console.log("received: %s", data);
   });
 
-  ws.send("hello");
+  ws.send(
+    JSON.stringify({
+      service: "ble",
+      id: "test1234",
+      data: {
+        export: "start_scan",
+        arguments: ["test", "test234"],
+      },
+    })
+  );
+
+  await new Promise((r) => setTimeout(r, 20000));
+  ws.send(
+    JSON.stringify({
+      service: "ble",
+      id: "newId",
+      data: {
+        export: "peripherals",
+        arguments: [],
+      },
+    })
+  );
+
+  await new Promise((r) => setTimeout(r, 3000));
+
+  ws.send(
+    JSON.stringify({
+      service: "ble",
+      id: "newId",
+      data: {
+        export: "connect",
+        arguments: ["22:FA:9F:3F:EB:16"],
+      },
+    })
+  );
+
+  await new Promise((r) => setTimeout(r, 3000));
+
+  ws.send(
+    JSON.stringify({
+      service: "ble",
+      id: "newId",
+      data: {
+        export: "discover_services",
+        arguments: [],
+      },
+    })
+  );
+
+  await new Promise((r) => setTimeout(r, 3000));
+
+  ws.send(
+    JSON.stringify({
+      service: "ble",
+      id: "newId",
+      data: {
+        export: "write_to_uuid",
+        arguments: [0xffd9, [0x56, 0xff, 0x00, 0x00, 0x00, 0xf0, 0xaa]],
+      },
+    })
+  );
+
+  await new Promise((r) => setTimeout(r, 3000));
+
+  ws.send(
+    JSON.stringify({
+      service: "core",
+      id: "newId",
+      data: {
+        export: "restart",
+        arguments: [0xffd9, [0x56, 0xff, 0x00, 0x00, 0x00, 0xf0, 0xaa]],
+      },
+    })
+  );
+
   console.log("new connection");
 });
