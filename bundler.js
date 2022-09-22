2b272d06425465230250a939a288807ffaa4ce99
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-09-22 19:58:08 +0200

    don't send the authentication string

--- bundler.js
+++ bundler.js
@@ -3,12 +3,9 @@ import config from "@proxtx/config";
 
 export const request = async (id, request) => {
   if (clients[id]) {
-    if (
-      request.service == "core" &&
-      request.data?.export == "key" &&
-      request.auth != config.auth
-    )
-      return { success: false, error: 2 };
+    if (request.service == "core" && request.data?.export == "key")
+      if (request.auth != config.auth) return { success: false, error: 2 };
+      else request.auth = "auth_guarantee";
     return { success: true, result: await clients[id].sendRequest(request) };
   } else return { success: false, error: 1 };
 };
